5d1efcf2b027b7488e138594770d64084063549d
Brendan Doherty <brendan@urbanquarters.co.nz>
2022-08-06 09:10:05 +1200

    update example

--- index.js
+++ index.js
@@ -2,19 +2,25 @@
 
 // See https://github.com/actualbudget/actual/blob/master/docs/API.md
 
+// node index.js https://your-server.fly.dev/ Your-Budget-Id
+
 let serverURL = process.argv[2];
 let budgetId = process.argv[3];
+let dataDir = process.cwd();
 console.log(`serverURL: ${serverURL}`);
 console.log(`budgetId: ${budgetId}`);
 
 let actual = require('@actual-app/api');
 
-await actual.init({
+actual.init({
   config: {
-    serverURL: serverURL
+    serverURL: serverURL,
+    dataDir: dataDir
   }
 });
 
-await actual.internal.send('load-budget', { id: budgetId });
+//actual.internal.send('load-budget', { id: budgetId });
+ actual.loadBudget(budgetId);
 
-await actual.getAccounts();
+let p = actual.getPayees();
+console.log(p);
